f418a60683e06c7feca5547ea9d37e4d597e21dc
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-05-25 15:29:18 -0300

    feat: criado classe UserService com método create

---
+++ src/services/User/UserService.ts
@@ -0,0 +1,16 @@
+import type IUserModel from '../../Repository/User/IUserModel'
+import type IUser from '../../interfaces/IUser'
+import type IUserService from './IUserService'
+import crypt from '../../utils/cryptPassword'
+
+export default class UserService implements IUserService {
+  private readonly _model: IUserModel
+
+  constructor (model: IUserModel) {
+    this._model = model
+  }
+
+  async create (user: IUser): Promise<void> {
+    await this._model.create({ ...user, password: await crypt(user.password) })
+  }
+}
