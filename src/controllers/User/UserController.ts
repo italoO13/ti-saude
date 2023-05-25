f11b7498b92cca2badf11a9f9a5d349ce28bad7c
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-05-25 15:30:01 -0300

    feat: criado classe UserController com método create

---
+++ src/controllers/User/UserController.ts
@@ -0,0 +1,20 @@
+import { type Request, type Response, type NextFunction } from 'express'
+import type IUserController from './IUserController'
+import type IUserService from '../../services/User/IUserService'
+
+export default class UserController implements IUserController {
+  private readonly service: IUserService
+
+  constructor (service: IUserService) {
+    this.service = service
+  }
+
+  create = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    try {
+      await this.service.create(req.body)
+      return res.status(201).json({ message: 'Created' })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
