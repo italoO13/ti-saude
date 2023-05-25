2a03e7cc4f51fd9fe3a5077538fab58556fe1eba
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-05-25 09:27:14 -0300

    chore: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-const PORT = 3000;
+const PORT = 3000
 
-const server = app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
-));
+const server = app.listen(PORT, () => {
+  console.log(
+  `Server is running on PORT: ${PORT}`
+  )
+})
 
-export default server;
+export default server
