db1d80a0a8b28a3b9a7f3788c659b014b10c027b
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-05-25 14:36:11 -0300

    feat: criado class UserModel com método create em Repository

---
+++ src/Repository/User/UserModel.ts
@@ -0,0 +1,34 @@
+import type IUser from '../../interfaces/IUser'
+import type IUserModel from './IUserModel'
+import { prismaClient } from '../../database/prismaClient'
+
+export default class UserModel implements IUserModel {
+  private readonly model = prismaClient
+
+  private async validatedUser (email: string, crm: string): Promise<void> {
+    const existingUser = await this.model.user.findFirst({
+      where: {
+        OR: [
+          { email },
+          { crm }
+        ]
+      }
+    })
+    if (existingUser != null) {
+      throw Error('Email ou crm já cadastrados !')
+    }
+  }
+
+  async create (user: IUser): Promise<void> {
+    const { email, crm, name, password } = user
+    await this.validatedUser(email, crm)
+    await this.model.user.create({
+      data: {
+        email,
+        crm,
+        name,
+        password
+      }
+    })
+  }
+}
