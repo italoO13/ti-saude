3cf6248e3a64a09af114e5cf5af45203bd2f7e83
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-05-26 08:18:36 -0300

    fix: erro de tipagem do metodo create

--- src/controllers/User/IUserController.ts
+++ src/controllers/User/IUserController.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response, type NextFunction } from 'express'
 
 export default interface IUserController {
-  create: (res: Request, req: Response, next: NextFunction) => Promise<Response>
+  create: (res: Request, req: Response, next: NextFunction) => Promise<void>
 }
