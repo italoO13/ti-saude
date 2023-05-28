ee4f95c74b8f5ff2b9addd691cec7a273937e590
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-05-28 09:17:43 -0300

    chore: add migration e schema de pacient

---
+++ prisma/migrations/20230528121628_add_post/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `pacients` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `pacients_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `pacients` ADD CONSTRAINT `pacients_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
