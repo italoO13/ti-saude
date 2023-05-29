55f179bfbdaacda6a3781b413994dcd287109bc0
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-05-29 11:10:30 -0300

    fix: retirando um dos testes

--- src/tests/integ/pacient.test.ts
+++ src/tests/integ/pacient.test.ts
@@ -183,14 +183,6 @@ describe('Teste da rota Pacients', () => {
         expect(result.status).to.equal(404)
         expect(result.body).to.deep.equal({ message: 'Pacient not exists' })
       })
-      it('Retorna status 404 quando Usuário não existe', async () => {
-        sinon.stub(FactoryPrisma.prototype, 'findById').resolves(null)
-        const result = await chai.request(app).put('/pacients/1').send(mocks.pacient)
-          .set('authorization', mocks.token)
-
-        expect(result.status).to.equal(404)
-        expect(result.body).to.deep.equal({ message: 'User not exists' })
-      })
     })
   })
 })
